5c4aa0c82a02c1232775436f2fd8c2ae15e07329
CamIAm <rcmontgo@gmail.com>
2017-12-13 09:37:40 -0500

    post and set payment requests impl.

--- app/src/action-creators/request.js
+++ app/src/action-creators/request.js
@@ -14,26 +14,53 @@ import {
   concat
 } from "ramda"
 
-//const url = process.env.REACT_APP_BASE_URL
+const url = process.env.REACT_APP_BASE_URL
 
 export const createRequest = async(dispatch, getState) => {
-  let txsToPost = getState().transactionForm
+  let requestToPost = getState().transactionForm
   const activeUser = getState().activeUser
   // requestee which is passed in can be replaced with txsToPost.recipient
-  txsToPost = merge(txsToPost, {
+  requestToPost = merge(requestToPost, {
     'timeStamp': Date
       .now()
       .toString(),
     "currency": "USDTEST",
-    "requestee": txsToPost.recipient,
+    "requestee": requestToPost.recipient,
     "requester": activeUser.id
   })
+  const response = await fetch(`${url}/requests`, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify(requestToPost)
+  }).then(res => res.json())
+
+  console.log("response in createTxs: ", response)
+
+  if (!response.ok) {
+    dispatch({type: ERROR, payload: 'Could not add request'})
+    return
+  }
+  dispatch(setAllRequests)
   // payment requests should have their own api endpoints eventually
-  console.log("creating request in action-creator: ", txsToPost)
-  dispatch({type: SET_PAYMENT_REQUEST, payload: txsToPost})
-  console.log("create-request after dispatch")
-  console.log("payload in create-request", txsToPost)
-  // console.log(`/profile/${activeUser.id}`) clear form
+  // console.log("creating request in action-creator: ", txsToPost)
+  // dispatch({type: SET_PAYMENT_REQUEST, payload: requestToPost})
   dispatch({type: CLEAR_SEND_FORM})
   history.push(`/profile/${activeUser.id}`)
+}
+
+export const setAllRequests = async(dispatch, getState) => {
+  const response = await fetch(`${url}/requests`)
+    .then(res => res.json())
+    .catch(err => console.log('err: ', err));
+  // if (!response) {     console.log("inside !response.ok",response)     return }
+  console.log("the response setAllTransactions: ", response)
+  const sortByTimeStamp = sortBy(prop('timeStamp'))
+  dispatch({
+    type: SET_PAYMENT_REQUEST,
+    payload: reverse(sortByTimeStamp(response))
+  })
+  // THIS dispatch my need to be modified to establish a bool flag value for
+  // payment request dispatch({type: DATA_LOADED, payload: true})
 }
